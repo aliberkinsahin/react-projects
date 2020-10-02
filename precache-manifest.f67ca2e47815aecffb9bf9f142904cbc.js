4b88325619e422004539a6eff0891da24daf9c52
Ali Berkin Şahin <aliberkinsahin@gmail.com>
2020-10-02 20:08:00 +1000

    Updates

---
+++ precache-manifest.f67ca2e47815aecffb9bf9f142904cbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce96c8206f258db765414f69240c6313",
+    "url": "/react-projects/index.html"
+  },
+  {
+    "revision": "9bc87fce4c77fb44423d",
+    "url": "/react-projects/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "475170cf8c719d6d2f53",
+    "url": "/react-projects/static/js/2.eab42bbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-projects/static/js/2.eab42bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bc87fce4c77fb44423d",
+    "url": "/react-projects/static/js/main.fa5b9739.chunk.js"
+  },
+  {
+    "revision": "90b3f9018640d835ef5f",
+    "url": "/react-projects/static/js/runtime-main.34ba5d3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-projects/static/media/logo.5d5d9eef.svg"
+  }
+]);
